e82fd6e9a01695beeb3823c7b0e1eb5bbde2f2d9
ruben <rubenhussong@gmai.com>
2021-04-15 16:05:02 +0200

    changed shop link

--- src/app/core/config/config.ts
+++ src/app/core/config/config.ts
@@ -1,4 +1,4 @@
 export const BOTTLE_DETAILS = {
     price: "29,99€",
-    shopUrl: "https://www.ebay.de/itm/5b4-nachhaltige-Trinkflasche-1L-auslaufsicher-BPA-Frei-/124506909511"
+    shopUrl: "https://www.decathlon.de/p/mp/5b4/trinkflasche-nachhaltig-auslaufsicher-1l-bpa-frei/_/R-p-febf9602-fd01-4e0a-807f-d1052674c494?mc=febf9602-fd01-4e0a-807f-d1052674c494_c251&c=GRAU"
 }
